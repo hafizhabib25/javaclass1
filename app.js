d252adb67a3e53d8ce321efbb793bfeda17e34ac
hafiz habib25 <thehabib25112001@gmail.com>
2024-09-08 20:51:11 +0500

    add files

---
+++ app.js
@@ -0,0 +1,77 @@
+// chapter no 1 ALERT
+
+// alert("error please enter a valid password") 1
+
+
+
+// alert("welcome to js land...\n happy coding")   2
+
+
+
+// alert("welcome to js land...")   3
+
+
+// alert("happy coding!")   4
+
+
+
+// alert("hello ...i can run js trough my web browser's console")   5
+
+
+
+
+// chapter no 2 variables for string
+
+//q no 1
+var username; 
+console.log(username)
+
+//q no 2
+var myname = "habib"  
+console.log(myname)
+//q no 3
+var messege;  
+messege = "hello world";
+alert(messege)
+
+//q no 4
+var name = "john doe"
+alert(name)
+
+
+var age = "15 year old"
+alert(age)
+
+
+var bakwas = "jaohn doe bakwas banda ha"
+alert(bakwas)
+
+//q no 5
+
+
+
+var js = "pizza\n pizz \n piz \n pi \n p"
+alert(js)
+
+//q no 6
+
+
+var email = "thehabib25112001@gmail.com"
+alert(email)
+
+
+
+//q no 7
+
+var javascript = "i m trying to learn from the book a smarter wy to learn javascript"
+alert(javascript)
+
+
+
+// chapter no 3 
+
+//q no 1
+
+
+var age = 20;
+alert()
